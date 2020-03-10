1d03e6885bbbdef46a4cd269ea2e02a1804a73e0
AntoineDuf <antoine.dufayet@gmail.com>
2020-03-10 19:02:15 +0100

    Creation of the project.

---
+++ Reciplease/Reciplease/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Antoine Dufayet on 10/03/2020.
+//  Copyright © 2020 NatProd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
