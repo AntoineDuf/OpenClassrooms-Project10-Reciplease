763fd602591f49e1e1ec10aa133eda7818049112
AntoineDuf <antoine.dufayet@gmail.com>
2020-03-29 14:24:32 +0200

    Add of UIViewController extension with the method with UIAlertController.

---
+++ Reciplease/Reciplease/Extensions/UIViewController+UIAlertController.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+UIAlertController.swift
+//  Reciplease
+//
+//  Created by Antoine Dufayet on 20/03/2020.
+//  Copyright © 2020 NatProd. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+/// Method that permit to display the error alert window.
+    func alert(title: String? = nil, message: String? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style:.default, handler: nil)
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+}
