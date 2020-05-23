669fa5dea64a738315bcae71886713944d73b9e5
AntoineDuf <antoine.dufayet@gmail.com>
2020-05-23 11:25:21 +0200

    Refacto in WebView interface.

--- Reciplease/Reciplease/WebView/WebViewController.swift
+++ Reciplease/Reciplease/WebView/WebViewController.swift
@@ -8,20 +8,28 @@
 
 import UIKit
 import WebKit
+import CoreData
 
 class WebViewController: UIViewController {
     var viewModel: WebViewModel!
 
     @IBOutlet private weak var webView: WKWebView!
+    @IBOutlet weak var favoriteButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = viewModel.getUrl()
-        let request = URLRequest(url: URL(string: url)!)
+        let request = viewModel.getUrl
         webView.load(request)
+        favoriteButton.image = viewModel.favoriteButtonImage
     }
 
     @IBAction func favoriteButton(_ sender: Any) {
-        viewModel.saveRecipe()
+        if favoriteButton.image == UIImage(named: Asset.heart.name) {
+            viewModel.saveRecipe()
+            favoriteButton.image = UIImage(named: Asset.heartFill.name)
+        } else {
+            viewModel.deleteRecipe()
+            favoriteButton.image = UIImage(named: Asset.heart.name)
+        }
     }
 }
--- Reciplease/Reciplease/WebView/WebViewModel.swift
+++ Reciplease/Reciplease/WebView/WebViewModel.swift
@@ -8,31 +8,82 @@
 
 import Foundation
 import CoreData
+import UIKit
 
-struct WebViewModel {
-    private let recipe: Hit
-    var openBy: State
+class WebViewModel {
+    let recipe: Hit?
+    let favoriteRecipe: MyRecipe?
+    private var openBy: State
+    var url: String?
+    private let stack: CoreDataManager
 
-    init(recipe: Hit, openBy: State) {
+    init(recipe: Hit?,
+         favoriteRecipe: MyRecipe?,
+         openBy: State,
+         stack: CoreDataManager = CoreDataManager(coreDataStack: .init(modelName: "Reciplease"))) {
         self.recipe = recipe
+        self.favoriteRecipe = favoriteRecipe
         self.openBy = openBy
+        self.stack = stack
     }
 
-    func getUrl() -> String {
-        return recipe.recipe.url
+    var getUrl: URLRequest {
+        let request: URLRequest
+        let url: URL
+        switch openBy {
+        case .openByFavoriteList:
+            url = URL(string: (favoriteRecipe?.url)!)!
+        case .openByRecipeList:
+            url = URL(string: (recipe?.recipe.url)!)!
+        }
+        request = URLRequest(url: url)
+        return request
     }
 
     func saveRecipe() {
-        let myRecipe = MyRecipe(context: AppDelegate.viewContext)
-        myRecipe.name = recipe.recipe.label
-        myRecipe.time = recipe.recipe.totalTime
-        myRecipe.image = recipe.recipe.image
-        myRecipe.url = recipe.recipe.url
-        try? AppDelegate.viewContext.save()
+        switch openBy {
+        case .openByFavoriteList:
+            guard let recipe = favoriteRecipe else { return }
+            stack.makeRecipe(name: recipe.name!, totalTime: recipe.time, image: recipe.image!, url: recipe.url!)
+        case .openByRecipeList:
+            guard let recipe = self.recipe?.recipe else { return }
+            stack.makeRecipe(name: recipe.label, totalTime: recipe.totalTime, image: recipe.image, url: recipe.url)
+        }
     }
-    
+
     enum State {
         case openByRecipeList
         case openByFavoriteList
+    }
+
+    func deleteRecipe() {
+        let recipeName: String
+        switch openBy {
+        case .openByFavoriteList:
+            guard let name = favoriteRecipe?.name else { return }
+            recipeName = name
+        case .openByRecipeList:
+            guard let name = recipe?.recipe.label else { return }
+            recipeName = name
+        }
+        stack.deleteRecipe(recipeTitle: recipeName)
+    }
+
+    var favoriteButtonImage: UIImage {
+        let optionalName: String?
+        switch openBy {
+        case .openByFavoriteList:
+            optionalName = favoriteRecipe?.name
+        case .openByRecipeList:
+            optionalName = recipe?.recipe.label
+        }
+        guard let name = optionalName else { return Asset.heart.image }
+        let isRecipeInDatabase = self.isRecipeInDatabase(for: name)
+        let heartImage = isRecipeInDatabase ? Asset.heartFill.image : Asset.heart.image
+        return heartImage
+    }
+
+    private func isRecipeInDatabase(for name: String) -> Bool {
+        return stack.checkIfRecipeIsFavorite(recipeTitle: name)
     }
 }
